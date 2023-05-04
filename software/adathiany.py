dca64f01bb5ece2667ab1b9c32c17a121ab3b4c8
Szilágyi Gábor <gbr.szilagyi@gmail.com>
2023-05-04 09:35:36 +0200

    még mindig nem jutottam a végére a cost-nak

--- software/adathiany.py
+++ software/adathiany.py
@@ -37,10 +37,10 @@ class Spec:
 		# array of overall S21 values at the
 		# frequency sample points
 		S21 = []
-		ss = "shunt" # start with shunt component
 		for i in range(n+1):
 			# S-matrix of the current element
 			S = np.matrix([[1,0],[0,1]])
+			ss = "shunt" # start with shunt component
 			for j in range(len(values)):
 				if types[j] == 'L':
 					Z = faxis[i]*1j*2*pi*values[j]
@@ -57,8 +57,16 @@ class Spec:
 						[2*Y0, -Y]])/(Y+2*Y0)
 					ss = "shunt"
 				S=S*m
-			S21.append(S.item(0,1))
+			S21.append(abs(S.item(0,1)))
 		#plt.plot(faxis, S21)
+		#plt.show()
+		cost=0
+		for i in len(self.starts):
+			for j in len(faxis):
+				if faxis[j]>self.starts[i] && faxis[j]<self.ends[i]:
+					if self.directions[i] == "pass"
+						cost += max(0, S21[j]/self.limits[i])
+					else: # "stop"
 
-spec = Spec([10, 20, 50], [12, 22, 52], [1,1,1], ["pass", "stop", "pass"])
-spec.cost(['L', 'C', 'L'], [1, 1, 1])
+spec = Spec([0.01], [1], [1], ["pass"])
+spec.cost(['L', 'L', 'C'], [1, 0, 0.1])
